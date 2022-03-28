e4dc06a13ec2820a5ebb01558d51fd4f0257d961
hunglo67 <71253293+hunglo67@users.noreply.github.com>
2022-03-28 11:00:32 +0300

    geek

---
+++ 3_Input(Laynhieugiatridauvao)/N1_UsingList.py
@@ -0,0 +1,10 @@
+# lay 2 gia tri a va b
+x, y = input("Enter two values: ").split()
+print("Values 1: ",x)
+print("Values 1: ",y)
+#In ra dung format
+print("First number is {}  and second number is {}".format(x,y))
+#Lay nhieu gia tri va tao thanh 1 list
+a = list(map(int, input("Enter multiple values: ").split()))
+print(a)
+
---
+++ 3_Input(Laynhieugiatridauvao)/N2_UsingListComprehension.py
@@ -0,0 +1,8 @@
+#Lay 2 gia tri dau vao
+x, y = [int(x) for x in input("Enter two number: ").split()]
+print("Values 1: ", x)
+print("Values 2: ", y)
+print("Values 1: {} and values 2: {}".format(x,y))
+#Lay nhieu gia tri tao thanh list
+x = [int(x) for x in input("Enter multiple values: ").split()]
+print(x)
